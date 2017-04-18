d8be0a2d1be23e1ccdb3d425cf8f5ef83c876385
jakob-home <jakobdellago@gmail.com>
2017-04-18 22:07:56 +0200

    letzte bugbehebung

--- suche.php
+++ suche.php
@@ -38,15 +38,15 @@
             <br><input type="submit" class="signupbtn" value="Suchen"><br>
             <?php 
                 
-                //if(isset($_GET['suche'])){
+                if(isset($_GET['suche'])){
             
-                while($result!=null){
-                        echo("<br><img src=Kunstwerke/". $result['w_bilddatei'] ." alt='Das gewünschte Bild existiert in der Datenbank nicht'>");
-                        echo("<br><a href='werk.php?titel=". $result['titel']. "'</a>");
-                        echo("<br>".$result['titel']."<br>");
-                        $result = $statement->fetch();
+                    while($result!=null){
+                            echo("<br><img src=Kunstwerke/". $result['w_bilddatei'] ." alt='Das gewünschte Bild existiert in der Datenbank nicht'>");
+                            echo("<br><a href='werk.php?titel=". $result['titel']. "'</a>");
+                            echo("<br>".$result['titel']."<br>");
+                            $result = $statement->fetch();
+                    }
                 }
-                    
             
             
             ?>
